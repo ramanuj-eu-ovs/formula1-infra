b5c55723d68674cde6582ebe86143706fc5b292d
Sagarnil Dasgupta <152617646+sagarnildasgupta@users.noreply.github.com>
2025-06-07 22:55:42 +0200

    try4

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ module "resource_group" {
   tags                = var.tags
 }
 
-module "container_registry" {
-  #checkov:skip=CKV_AZURE_137:Ensure ACR admin account is disabled
-  source                     = "./modules/container_registry"
-  container_registry_name    = module.naming_container_registry.container_registry.name
-  resource_group_name        = module.resource_group.resource_group_name
-  location                   = var.location
-  admin_enabled              = true // Required by ML workspace
-  tags                       = var.tags
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-}
+# module "container_registry" {
+#   #checkov:skip=CKV_AZURE_137:Ensure ACR admin account is disabled
+#   source                     = "./modules/container_registry"
+#   container_registry_name    = module.naming_container_registry.container_registry.name
+#   resource_group_name        = module.resource_group.resource_group_name
+#   location                   = var.location
+#   admin_enabled              = true // Required by ML workspace
+#   tags                       = var.tags
+#   log_analytics_workspace_id = var.log_analytics_workspace_id
+# }
 
 module "vnet" {
   source              = "./modules/vnet"
