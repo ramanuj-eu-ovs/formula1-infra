50ca71daea8af52ea44b5f40a84c5bbc63d047f4
Sagarnil Dasgupta <152617646+sagarnildasgupta@users.noreply.github.com>
2025-06-07 22:53:45 +0200

    try4

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "resource_group" {
   source = "./modules/resource_group"
   #resource_group_name = module.naming.naming_container_registry.resource_group_name
-  resource_group_name = "rg-DAP-ops" #until the resource_group naming is fixed
+  resource_group_name = "rg-formula1-tst" #until the resource_group naming is fixed
   location            = var.location
   tags                = var.tags
 }
@@ -32,23 +32,3 @@ module "vnet" {
   }
 }
 
-/* module "github_runner" {
-  count                      = var.runner_registration_token != "" ? 1 : 0
-  source                     = "./archetypes/linux_vm_github_runner"
-  resource_group_name        = module.resource_group.resource_group_name
-  subnet_id                  = module.vnet.subnets["subnet-runner"].id
-  naming                     = module.naming_github_runner.settings
-  log_analytics_workspace_id = var.log_analytics_workspace_id
-  vnet_rules = [
-    { name = "github-runner-subnet",
-    id = module.vnet.subnets["subnet-runner"].id },
-    { name : "github-action-runner-vnet", # TODO: Old runner need to be removed 
-      id : "/subscriptions/2104e93b-4560-42ec-8c1b-8f18eda53627/resourceGroups/D1HPRGEW001/providers/Microsoft.Network/virtualNetworks/D1HP-vm-DAP-githubactionrunner-dev-vnet/subnets/default"
-    }
-  ]
-  aad_reader_ids = {
-    kdap_technical_owners = var.ops_technical_owners
-  }
-  tags                      = var.tags
-  runner_registration_token = var.runner_registration_token
-} */
